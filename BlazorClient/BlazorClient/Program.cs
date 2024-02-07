a87749059c0626837cb3053b6b80e655806bf5ad
Thomas ANDERSSON <thomas.andersson@soprasteria.com>
2024-02-07 10:27:22 +0100

    Add project files.

---
+++ BlazorClient/BlazorClient/Program.cs
@@ -0,0 +1,49 @@
+using BlazorClient.Client.Pages;
+using BlazorClient.Components;
+using Microsoft.AspNetCore.Builder;
+
+
+namespace BlazorClient
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveWebAssemblyComponents();
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddHttpClient();
+            builder.Services.AddSwaggerGen();
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseWebAssemblyDebugging();
+                // Configure the HTTP request pipeline.
+
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseAntiforgery();
+            //   app.UseEndpoints(endpoints=>
+            app.MapRazorComponents<App>()
+                .AddInteractiveWebAssemblyRenderMode()
+                .AddAdditionalAssemblies(typeof(Counter).Assembly);
+
+            app.Run();
+        }
+    }
+}
